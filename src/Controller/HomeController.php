f6a3bbf1e250daab74f06023ce6483082179f000
gouedard-mathias <m-gouedard40500@hotmail.fr>
2021-02-04 15:51:00 +0100

    home and profile done

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Repository\ClientRepository;
+use App\Repository\ExperienceRepository;
+use App\Repository\ProjectRepository;
+use App\Repository\SkillRepository;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,22 +19,34 @@ class HomeController extends AbstractController
     public function index(UserRepository $userRepository): Response
     {
         $user = $userRepository->findOneBy([]);
-        $userInfo = $user->getUserInfo();
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-            'user' => $userInfo,
+            'user' => $user,
         ]);
     }
 
     /**
      * @Route("/profile", name="profile")
      */
-    public function profile(UserRepository $userRepository): Response
+    public function profile(
+        UserRepository $userRepository,
+        ProjectRepository $projectRepository,
+        ClientRepository $clientRepository,
+        SkillRepository $skillRepository,
+        ExperienceRepository $experienceRepository
+    ): Response
     {
         $user = $userRepository->findOneBy([]);
-        $userInfo = $user->getUserInfo();
+        $projects = $projectRepository->findAll();
+        $clients = $clientRepository->findAll();
+        $skills = $skillRepository->findAll();
+        $experiences = $experienceRepository->findAll();
         return $this->render('home/profile.html.twig', [
-            'user' => $userInfo,
+            'user' => $user,
+            'projects' => $projects,
+            'clients' => $clients,
+            'skills' => $skills,
+            'experiences' => $experiences,
         ]);
     }
 }
